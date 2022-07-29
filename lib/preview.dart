b10f6c9c55a487bc06f56ced0dd76347f34b512b
Özgür Özdemir <ozgur@ozgurkurumsal.com>
2022-07-29 16:42:36 +0300

    Reçete eklendi.

--- lib/preview.dart
+++ lib/preview.dart
@@ -1,15 +1,29 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
+import 'package:share_plus/share_plus.dart';
 
 class Preview extends StatelessWidget {
-  const Preview({super.key});
+  final String path;
+  const Preview({super.key, required this.path});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: SfPdfViewer.network(
-          'https://cdn.syncfusion.com/content/PDFViewer/flutter-succinctly.pdf'),
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('REÇETE DETAYI'),
+      ),
+      // body: Center(child: Text(path)),
+      body: SfPdfViewer.file(File(path)),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.share),
+        onPressed: () async => await Share.shareFiles([path]),
+        // 'check out my website https://example.com',
+        // subject: 'Look what I made!'),
+      ),
+      //     'https://cdn.syncfusion.com/content/PDFViewer/flutter-succinctly.pdf'),
     );
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,11 +6,13 @@ import FlutterMacOS
 import Foundation
 
 import path_provider_macos
+import share_plus_macos
 import syncfusion_pdfviewer_macos
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SyncfusionFlutterPdfViewerPlugin.register(with: registry.registrar(forPlugin: "SyncfusionFlutterPdfViewerPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
